a937a331013702e8ffc6c0a415f44d32dd356f8b
gtaumaturgo <gtaumaturg@gmail.com>
2019-03-23 16:17:50 -0300

    added instructions types

---
+++ instruction_decoder.cpp
@@ -0,0 +1,6 @@
+#include "instruction_decoder.h"
+
+instruction_decoder::instruction_decoder()
+{
+
+}
---
+++ instruction_decoder.h
@@ -0,0 +1,11 @@
+#ifndef INSTRUCTION_DECODER_H
+#define INSTRUCTION_DECODER_H
+
+
+class instruction_decoder
+{
+public:
+    instruction_decoder();
+};
+
+#endif // INSTRUCTION_DECODER_H
